fed1370fe09814abb458df58a727d0a1faf255ba
qq_40159409 <qq_40159409@noreply.gitcode.com>
2025-05-21 14:31:11 +0800

    数据导入功能

---
+++ dataimporter.cpp
@@ -0,0 +1,3 @@
+#include "dataimporter.h"
+
+dataImporter::dataImporter() {}
---
+++ dataimporter.h
@@ -0,0 +1,10 @@
+#ifndef DATAIMPORTER_H
+#define DATAIMPORTER_H
+
+class dataImporter
+{
+public:
+    dataImporter();
+};
+
+#endif // DATAIMPORTER_H
